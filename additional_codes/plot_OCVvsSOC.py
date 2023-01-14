e70f432e656067d0ab65b7ec08f295113bbbf0cc
MohamedElsyed2 <95366105+MohamedElsyed2@users.noreply.github.com>
2023-01-14 09:54:00 +0100

    update

--- additional_codes/plot_OCVvsSOC.py
+++ additional_codes/plot_OCVvsSOC.py
@@ -4,32 +4,17 @@ import numpy
 def charging_voltage_charging_tim_plot():
 
     # charging_tim axis values
-    charging_tim = [94.5 , 104 , 116.5, 135 , 175]       # X-axis values
-    charging_voltage = [2560 , 2750, 2880 , 2980, 3080]      # Y-axis values
+    SOC = [0.00,5.00,10.00,15.00,20.00,25.00,30.00,35.00,40.00,45.00,50.00,55.00,60.00,65.00,70.00,75.00,80.00,85.00,90.00,95.00,100.00]    # SOC axis values
+    OCV = [3.44,3.47,3.52,3.55,3.58,3.6,3.61,3.63,3.64,3.67,3.68,3.72,3.75,3.8,3.84,3.88,3.94,3.99,4.04,4.11,4.18]  
     # charging_tim = [0.00 , 94.5]    # X-axis values
     # capacity_axis = [0.0, 2560 ] 
     global fig
     fig, curve = plt.subplots()
-    curve.plot(charging_tim, charging_voltage, color="blue", marker='o', markerfacecolor='red')
-    curve.set(xlabel='charging_tim %', ylabel='charging_voltage (V)',title='The relation between charging_voltage and charging_tim')
-    # ymin = 2.4
-    # xmin = 0
-
-    time12= 30
-    #charging_timIntial= numpy.exp(numpy.interp([voltage], y[::-1], numpy.log(x[::-1])))
-    charging_volt =float("{:.4f}".format(numpy.interp(time12, charging_tim[::1], charging_voltage[::1])))
-    print(charging_volt)
-    voltage= 3.925
-    #charging_timIntial= numpy.exp(numpy.interp([voltage], y[::-1], numpy.log(x[::-1])))
-    charging_timIntial =float("{:.2f}".format(numpy.interp(voltage, charging_tim[::1], charging_voltage[::1])))
-    print(charging_timIntial)
-    # for xi, yi in [(charging_timIntial, y1), (x2, y2)]:
-    #     ax.hlines(yi, xmin, xi, color='r')
-    #     ax.vlines(xi, ymin, yi, color='r')
-    #     print(f'x-value corresponding to y={10}: {xi:.3f}')
-    curve.set_ylim(2000 , 3500)
-    curve.set_xlim(90 , 180)
-    #curve.set_xscale('log')
+    curve.plot(SOC, OCV, color="blue", marker='o', markerfacecolor='red')
+    curve.set(xlabel='State of Charge (SOC)', ylabel='Open Circuit Voltage (OCV) (V)',title='')
+    
+    curve.set_ylim(3.3 , 4.5)
+    curve.set_xlim(-5.00 , 105.00)
     curve.grid(True)
     plt.show()
 charging_voltage_charging_tim_plot()
--- cloud code/state_of_health.py
+++ cloud code/state_of_health.py
@@ -15,6 +15,10 @@ def get_state_of_health (cell_number):
         time.sleep(2)
         if cell_number < 4:
             def get_nominal_lifetime (cell_number):
+                """ This method is coded according to the published paper: Muenzel, V.; de Hoog, J.; Brazil, M.; Vishwanath, A.; Kalyanaraman,
+                S. A multi-factor battery cycle life prediction methodology for optimal battery management. 
+                """
+                #**********************************************************************************#
                 a = 0.0039
                 b = 1.95
                 c = 67.51
@@ -22,7 +26,7 @@ def get_state_of_health (cell_number):
                 temperature = 25                   # the battery temperature will be constant at 25° C using the thermal management unit.
                 nominal_temperature = 25
                 num_cycle_life_temp = (a*pow(temperature,3) - b*pow(temperature,2) + c*temperature + d)/(a*pow(nominal_temperature,3) - b*pow(nominal_temperature,2) + c*nominal_temperature + d)
-
+                #*************************************************************************************#
                 disch_current = 0                 # no discharging current.
                 nominal_disch_current = 1                       # from datasheet
                 e = 4464
@@ -40,6 +44,7 @@ def get_state_of_health (cell_number):
                 p = 0.03168
                 num_cycle_life_charging_current = (m*exp(n*charging_current)+o*exp(p*charging_current))/(m*exp(n*nominal_charging_current)+o*exp(p*nominal_charging_current))
 
+                #********* Start of nominal nominal cycle life due to varying temperature*************#
                 q = 1471
                 u = 0.3369
                 v = -2.295
@@ -57,11 +62,12 @@ def get_state_of_health (cell_number):
                 real_cycle_life = q+((u/(2*v))*(s+100*u)-200*t)*dod + s*average_SOC + t* pow(dod,2) + u*dod*average_SOC + v* pow(average_SOC,2)
                 nominal_cycle_life = q+((u/(2*v))*(s+100*u)-200*t)*nominal_dod + s*nominal_average_SOC + t* pow(nominal_dod,2) + u*nominal_dod*nominal_average_SOC + v* pow(nominal_average_SOC,2)
                 num_cycle_life_SOC_DOD = real_cycle_life / nominal_cycle_life
+                #****************************************************************************************#
 
-
-                nominal_cycle_life = 649                                        # from battery datasheet.
+                nominal_cycle_life = 649                                        # (day) from battery datasheet.
                 equivelant_battery_num_cycle_life = int (nominal_cycle_life * num_cycle_life_temp * num_cycle_life_disch_current * num_cycle_life_charging_current * num_cycle_life_SOC_DOD)
                 return equivelant_battery_num_cycle_life
+
             #******* Start of the code to calculate SoH based on the passage of time *********#
             def SOH_passage_of_time (cell_number):
                 today = date.today()
@@ -72,7 +78,8 @@ def get_state_of_health (cell_number):
                 nominal_lifetime = get_nominal_lifetime (cell_number)/365      # number of life cycles when battery is not being used / 365 (assume that the battery will be charged once every day)  
                 SoH_age = 1 - (battery_age_year / nominal_lifetime) 
                 return SoH_age
-            #******* End of the code to calculate SoH based on the passage of time********#
+            #****************************************************************************#
+
             #******* Start of the code to calculate SoH based on number of cycles *********#
             def SOH_num_of_cycles (cell_number):
                 file = open("E:/Masterarbeit/BMS-for-Electric-Vehicles-/cloud code/cell"+str(cell_number)+"_num_of_cycles.txt", "r")  
@@ -97,7 +104,7 @@ def get_state_of_health (cell_number):
                 k3 = 0.003557 + 0.002207 * temp_coeff + 0.002843 * c_rate_coeff + 0.001493 * temp_coeff * c_rate_coeff       # k3 accounts for the capacity loss due to C-rate.
                 soh_num_of_cycles_coeff = 1- (0.5*k1*pow(num_of_cycles,2)+k2 * num_of_cycles) - (k3*c_rate /nominal_capacity)
                 return soh_num_of_cycles_coeff 
-            #******* End of the code to calculate SoH based on number of cycles*********#
+            #*************************************************************************************#
             try:
                 file = open("E:/Masterarbeit/BMS-for-Electric-Vehicles-/cloud code/battery_usage.txt", "r")   # open the file 'temperature.txt' in raeding mode.
                 battery_being_used = float (file.read())
@@ -129,7 +136,7 @@ def get_state_of_health (cell_number):
                 file.write(str(total_SOH))
             finally:
                 file.close()
-        #***************************************************************#
+        #********************************************************************#
         elif cell_number == 4:
             soh = 0
             for cell_number in range(1,4):     # get the state of health of every cell.
