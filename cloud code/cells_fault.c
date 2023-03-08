deaedfa3574418e7f80536d360d8c74c788d0437
Mohamed Elsyed <mo.elsyed3@gmail.com>
2023-03-08 00:16:47 +0000

    updated

--- cloud code/cells_fault.c
+++ cloud code/cells_fault.c
@@ -3,33 +3,49 @@
 #include <stdbool.h>
 # include <string.h>
 
-uint8_t check_cells_fault (uint16_t voltage_sensor1, uint16_t voltage_sensor2, uint16_t voltage_sensor3, uint16_t module1_voltage_sensor){
+uint8_t check_cells_fault (uint16_t voltage_sensor1, uint16_t voltage_sensor2, uint16_t voltage_sensor3){
 
-    // int cell_error;
-    // if (cool_sys_is_ON ==1)
-    // {
-    //     if (cool_sys_current <= 300)
-    //     {
-    //         cell_error = 1;
-    //     }
-    //     else
-    //     {
-    //         cell_error= 0;
-    //     }
-    // }
-    // else
-    // {
-    //     cell_error= 0;
-    // }
+
+    uint8_t error_code;
+    bool cell1_fault = voltage_sensor1>= 4300 || voltage_sensor1 <= 2500;
+    bool cell2_fault = voltage_sensor2>= 4300 || voltage_sensor2 <= 2500;
+    bool cell3_fault = voltage_sensor3>= 4300 || voltage_sensor3 <= 2500;
     
-    // return cell_error;
+    if (cell1_fault){
+        error_code = 1;
+    }
+    if (cell2_fault){
+        error_code = 2;
+    }
+    if (cell3_fault){
+        error_code = 3;
+    }
+    if (cell1_fault && cell2_fault){
+        error_code = 12;
+    }
+    if (cell2_fault && cell3_fault){
+        error_code = 23;
+    }
+    if (cell1_fault && cell3_fault){
+        error_code = 13;
+    }
+    if (cell1_fault && cell2_fault && cell3_fault){
+        error_code = 123;
+    }
+    if (cell1_fault == 0 && cell2_fault == 0 && cell3_fault == 0){
+        error_code = 0;
+    }
+return error_code;
 }
 
 int main() {
 
-    
-    uint8_t cell_error = check_cells_fault (voltage_sensor1, voltage_sensor2, voltage_sensor3, module1_voltage_sensor);
+    uint16_t voltage_sensor1= 5000;
+    uint16_t voltage_sensor2 = 5000;
+    uint16_t voltage_sensor3 = 5000;
 
+    uint8_t cell_error = check_cells_fault (voltage_sensor1, voltage_sensor2, voltage_sensor3);
+    
     if (cell_error != 0)
     {
     char  txdata [35];
@@ -41,9 +57,14 @@ int main() {
     return 0;
 }
 
-
 /*
     cell_error:
-    000: cooling system is OK.
-    001 : cooling system is defect. 
+    00000: all cells are connected and there is no defect.
+    00001 : cell #1 is not connected, short circuited, over-charged or over-discharged.
+    00002 : cell #2 is not connected, short circuited, over-charged or over-discharged.
+    00003 : cell #3 is not connected, short circuited, over-charged or over-discharged.
+    000012: cells #1 and #2 are not connected, short circuited, over-charged or over-discharged.
+    000013: cells #1 and #3 are not connected, short circuited, over-charged or over-discharged.
+    000023: cells #2 and #3 are not connected, short circuited, over-charged or over-discharged.
+    
 */
